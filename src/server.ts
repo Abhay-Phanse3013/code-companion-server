1c30f1c77757d97e5e2049dac8ec28863d0be929
Abhay Phanse <52242762+Abhay-Phanse3013@users.noreply.github.com>
2023-08-07 00:04:25 +0530

    Maybe_Backend_running

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-import express, { Request, Response } from 'express';
-import { app_v1 } from './v1';
+//javascript code for implementing the concept of listening to Websockets with Express
+const express = require('express');
+const ws = require('ws');
 
 const app = express();
 
-app.use('/api/v1', app_v1);
+//A headless websocket server is set up that also prints any events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message));
+});
 
-app.listen(3000, () => {
-  console.log("Server listening on port: 3000");
+const server = app.listen(3000,() => console.log('Server running on port 3000'));
+server. on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
 });
